7442fc763ad837beea48e9c8069253246d978744
shickmanIV <samuel.hickman@wsu.edu>
2024-02-17 11:55:08 -0800

    Initial Commit

---
+++ Globetrotter's guide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Globetrotter's guide
+//
+//  Created by Samuel Hickman on 2/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Globetrotter's guide/Globetrotter_s_guideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Globetrotter_s_guideApp.swift
+//  Globetrotter's guide
+//
+//  Created by Samuel Hickman on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Globetrotter_s_guideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
